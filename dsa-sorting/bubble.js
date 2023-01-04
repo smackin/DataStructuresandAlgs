af42c56ef22186ef3e598fbdde734e54436643b9
Stephanie <smmackin12@gmail.com>
2023-01-04 10:32:18 -0500

    sorting

---
+++ dsa-sorting/bubble.js
@@ -0,0 +1,15 @@
+function bubbleSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]; 
+    }; 
+    for (let i= arr.lenghth; i > 0; i--) {
+        for (let j = 0; j< 1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr, j, j+1) ;
+            }
+        }
+    }
+    return arr;
+}
+
+module.exports = bubbleSort;
